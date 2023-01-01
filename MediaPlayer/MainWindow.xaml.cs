29619010ae52faebbdc1dd0758787c848f7cecbf
20120508 <htk02102002@gmail.com>
2023-01-01 14:30:40 +0700

    minisize

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -517,5 +517,17 @@ namespace MediaPlayerProject
                 IsRepeat = true;
             }
         }
+
+        private void Minisize_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Normal)
+            {
+                this.WindowState= WindowState.Minimized;
+            }
+            else if(this.WindowState == WindowState.Minimized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
     }   
 }
